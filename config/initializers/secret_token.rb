3224232e52fdad4c34a80ebda6426c30af1bb6ed
Ananth Ramanathan <ananthdotr@gmail.com>
2014-04-17 09:55:48 +0100

    first initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BLocalMvp::Application.config.secret_key_base = '68d690f3bf8b397f6dc6743300597edec88e3850353c2fa24637942b5dac07e28c789a2d3567d0a52b268eaecafd17fdbd69fd8e7da409899efd399f0c90c7a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BLocalMvp::Application.config.session_store :cookie_store, key: '_bLocal_mvp_session'
